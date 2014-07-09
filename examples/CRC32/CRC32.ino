1634b2144a1f84e422b4a8d638590c0efbbcb5de
Christopher Baker <me@christopherbaker.net>
2014-07-09 09:36:32 -0500

    Added simple example.

---
+++ examples/CRC32/CRC32.ino
@@ -0,0 +1,55 @@
+// =============================================================================
+//
+// Copyright (c) 2013 Christopher Baker <http://christopherbaker.net>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// =============================================================================
+
+
+#include <CRC32.h>
+
+
+void setup()
+{
+    Serial.begin(115200);
+}
+
+
+void loop()
+{
+    uint8_t buffer[10];
+
+    for (size_t i = 0; i < 10; ++i)
+    {
+        buffer[i] = i;
+    }
+
+    uint32_t checksum = CRC32::checksum(buffer, 10);
+
+    uint32_t knownChecksum = 1164760902;
+
+    Serial.print("Calculated: ");
+    Serial.print(checksum);
+    Serial.print("     Known: ");
+    Serial.print(knownChecksum);
+    Serial.println();
+
+    delay(1000);
+}
